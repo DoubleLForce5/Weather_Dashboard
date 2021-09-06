896016742df8bf01233ff724142b47564d680ed4
Liv <lopez.olivia17@outlook.com>
2021-09-06 14:56:24 -0500

    modified index.html and script.js

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,62 @@
 const key = config.API_KEY;
 const inputEl = document.querySelector(".input-box");
 const citySearchBtn = document.querySelector(".search-button");
-const searchHistory = document.createElement("li");
-searchHistory.setAttribute("class", "search-history");
-const invalidSearchMessage = document.createElement("p");
-invalidSearchMessage.setAttribute("class", "search-message");
 const searchContainer = document.querySelector(".search-container");
-const currentWeatherEl = document.querySelector('.current-weather');
-const fiveDayForecastEl = document.querySelector('.five-day');
+const currentWeatherEl = document.querySelector(".current-weather");
+const fiveDayForecastEl = document.querySelector(".five-day");
+const invalidSearchMessageEl = document.querySelector(
+  ".invalid-search-message"
+);
+const citySearched = document.createElement("button");
+citySearched.setAttribute("class", "city-searched");
 
-async function getLatLon(event) {
-  event.preventDefault();
 
-  const citySearchInput = inputEl.value.trim();
+function inputInfo(event) {
+  console.log('inputInfo', event)
+
+  const citySearchInput = inputEl.value.trim().toLowerCase();
 
   if (citySearchInput === "") {
-    searchContainer.appendChild(invalidSearchMessage);
-    invalidSearchMessage.textContent = "Please enter a city";
+    invalidSearchMessageEl.textContent = "Please enter a city!";
   } else {
-    invalidSearchMessage.textContent = "";
+    invalidSearchMessageEl.textContent = "";
+    getLatLon(citySearchInput);
+  }
+ 
+}
 
-    const citySearchedCoordinates = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${citySearchInput}&appid=${key}`
-    );
+function searchHistory(citySearchInput) {
+  currentWeatherEl.textContent = '';
+  fiveDayForecastEl.textContent = '';
 
+  citySearched.textContent = citySearchInput;
+  searchContainer.appendChild(citySearched);
+  inputEl.value = ''
+}
+
+citySearched.addEventListener('click', function(event){
+  event.preventDefault()
+  console.log('event', event)
+  getLatLon(event.target.innerText)
+})
+
+async function getLatLon(citySearchInput) {
+  const citySearchedCoordinates = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${citySearchInput}&appid=${key}`
+  );
+
+  if (citySearchedCoordinates.ok) {
+    searchHistory(citySearchInput);
     const coordinates = await citySearchedCoordinates.json();
 
-    getWeatherData(coordinates.coord.lat, coordinates.coord.lon, coordinates.name);
+    getWeatherData(
+      coordinates.coord.lat,
+      coordinates.coord.lon,
+      coordinates.name
+    );
+  } else {
+    invalidSearchMessageEl.textContent =
+      "There are no search results matching your criteria.";
   }
 }
 
@@ -39,48 +69,49 @@ async function getWeatherData(lat, lon, name) {
   const weather = await weatherData.json();
 
   console.log(weather);
-  
-  // current weather 
-  const date = new Date (weather.current.dt * 1000).toLocaleDateString('en-US')
-  const currentWeatherIcon = `https://www.openweathermap.org/img/wn/${weather.current.weather[0].icon}.png`
-  const currentUvIndex = 10
 
-  console.log(currentWeatherIcon)
-    currentWeatherEl.innerHTML = `
+  // current weather
+  const date = new Date(weather.current.dt * 1000).toLocaleDateString("en-US");
+  const currentWeatherIcon = `https://www.openweathermap.org/img/wn/${weather.current.weather[0].icon}.png`;
+  const currentUvIndex = 10;
+
+  console.log(currentWeatherIcon);
+  currentWeatherEl.innerHTML = `
     <div class="current-weather-header">${name} (${date})<img src="${currentWeatherIcon}" alt="weather-icon"/></div>
     <p class="current-temp">${weather.current.temp}<p>
     <p class="current-humidity">${weather.current.humidity}<p>
     <p class="current-wind-speed">${weather.current.wind_speed}<p>
-    <p class="current-uv">${currentUvIndex}<p>`
+    <p class="current-uv">${currentUvIndex}<p>`;
 
-    if(currentUvIndex < 3){
-    document.querySelector('.current-uv').style.color = '#3ea832'
-  } else if(currentUvIndex < 6) {
-    document.querySelector('.current-uv').style.color = '#ffa500'
-  } else if(currentUvIndex < 8){
-    document.querySelector('.current-uv').style.color = '#a83232'
-  } else if(currentUvIndex < 11){
-    document.querySelector('.current-uv').style.color = '#a83265'
+  if (currentUvIndex < 3) {
+    document.querySelector(".current-uv").style.color = "#3ea832";
+  } else if (currentUvIndex < 6) {
+    document.querySelector(".current-uv").style.color = "#ffa500";
+  } else if (currentUvIndex < 8) {
+    document.querySelector(".current-uv").style.color = "#a83232";
+  } else if (currentUvIndex < 11) {
+    document.querySelector(".current-uv").style.color = "#a83265";
   } else {
-    document.querySelector('.current-uv').style.color = '#3299a8'
+    document.querySelector(".current-uv").style.color = "#3299a8";
   }
 
-  for(let i = 1; i <= 5; i++){
-    let dailyWeatherCard = document.createElement('div')
-    dailyWeatherCard.setAttribute('class', 'daily-weather-card')
-    fiveDayForecastEl.appendChild(dailyWeatherCard)
+  for (let i = 1; i <= 5; i++) {
+    let dailyWeatherCard = document.createElement("div");
+    dailyWeatherCard.setAttribute("class", "daily-weather-card");
+    fiveDayForecastEl.appendChild(dailyWeatherCard);
 
-    let fiveDayDate = new Date (weather.daily[i].dt * 1000).toLocaleDateString('en-US')
+    let fiveDayDate = new Date(weather.daily[i].dt * 1000).toLocaleDateString(
+      "en-US"
+    );
 
-    let fiveDayIcons = `https://www.openweathermap.org/img/wn/${weather.daily[i].weather[0].icon}.png`
+    let fiveDayIcons = `https://www.openweathermap.org/img/wn/${weather.daily[i].weather[0].icon}.png`;
 
     dailyWeatherCard.innerHTML = ` 
     <p>${fiveDayDate}</p>
     <img src="${fiveDayIcons}" alt="weather-icon"/> 
     <p>${weather.daily[i].temp.max}</p>
-    <p>${weather.daily[i].humidity}</p>`
+    <p>${weather.daily[i].humidity}</p>`;
   }
-
 }
 
-citySearchBtn.addEventListener("click", getLatLon);
+citySearchBtn.addEventListener("click", inputInfo);
